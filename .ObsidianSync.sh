8a845dc989788e6eb2b3ff99a132c4dabdcd6932
shinydisk <fshinydisk@proton.me>
2024-07-26 23:03:44 +0200

    Automatically ObsidianBrain synchronization.

--- .ObsidianSync.sh
+++ .ObsidianSync.sh
@@ -3,22 +3,22 @@
 # Add changes to the Git index
 git add .
 if [ $? -ne 0 ]; then
-  echo -e "❌ \e[31mError: Unable to add files.\e[0m"
+  echo -e "❌ Error: Unable to add files."
   exit 1
 fi
 
 # Commit the changes
 git commit -m "Automatically ObsidianBrain synchronization."
 if [ $? -ne 0 ]; then
-  echo -e "❌ \e[31mError: Unable to commit changes. Make sure there are changes to commit.\e[0m"
+  echo -e "❌ Error: Unable to commit changes. Make sure there are changes to commit."
   exit 1
 fi
 
 # Push the changes to the remote repository
 git push
 if [ $? -ne 0 ]; then
-  echo -e "❌ \e[31mError: Unable to push changes to the remote repository.\e[0m"
+  echo -e "❌ Error: Unable to push changes to the remote repository."
   exit 1
 fi
 
-echo -e "✅ \e[32mSynchronization with GitHub completed successfully.\e[0m"
+echo -e "✅ Synchronization with GitHub completed successfully."
